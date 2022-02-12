bab1ac99b05f8d61f29225bff45643cada4b4b6a
Albertabrahams <halilibrahim.soyman@gmail.com>
2022-02-12 17:58:37 +0300

    added

---
+++ index.js
@@ -0,0 +1,8 @@
+const fruits = ["Banana", "Orange","Lemon"];
+const vegetables = ['Broccoli','Celery','Parsley','Artichoke']
+
+console.log(fruits.concat(vegetables));
+console.log(fruits.concat("fig",3,"pear"));
+
+console.log(""+fruits);
+
